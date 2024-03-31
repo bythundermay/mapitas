f0d97ecb8738128cc95f6aed4fbea69ae63c12c8
Maria <mariarivas1422@gmail.com>
2024-03-31 19:15:00 +0200

    componentes, servicios, interfaces y rutas
    elementos iniciaes

---
+++ src/app/pages/countryview/countryview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-countryview',
+  standalone: true,
+  imports: [],
+  templateUrl: './countryview.component.html',
+  styleUrl: './countryview.component.css'
+})
+export class CountryviewComponent {
+
+}
